6119105ae1c9b18a59f757efc26230d5d5a41e9b
Andrew Hassing <hassiar@huskers.unl.edu>
2021-03-25 06:59:02 -0500

    creating the maven project

---
+++ src/main/java/com/hassing/dictionary/App.java
@@ -0,0 +1,13 @@
+package com.hassing.dictionary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
